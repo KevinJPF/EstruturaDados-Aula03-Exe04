8a50cb9d299f1d9905283dd380587d3df3ffe9c0
KevinJPF <123981519+KevinJPF@users.noreply.github.com>
2023-09-28 13:10:52 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Exercicio-04/exe04.c
@@ -0,0 +1,50 @@
+#include "pilha.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int prio(char o) {
+  switch (o) {
+  case '(':
+    return 0;
+  case '&':
+  case '|':
+    return 1;
+  case '!':
+    return 2;
+  }
+  return -1; // operador inválido!
+}
+
+char *posfixa(char *e) {
+  static char s[256];
+  int j = 0;
+  Pilha P = pilha(256);
+  for (int i = 0; e[i]; i++)
+    if (e[i] == '(')
+      empilha('(', P);
+    else if (e[i] == 'F' || e[i] == 'V')
+      s[j++] = e[i];
+    else if (strchr("!&|", e[i])) {
+      while (!vaziap(P) && prio(topo(P)) >= prio(e[i]))
+        s[j++] = desempilha(P);
+      empilha(e[i], P);
+    } else if (e[i] == ')') {
+      while (topo(P) != '(')
+        s[j++] = desempilha(P);
+      desempilha(P);
+    }
+  while (!vaziap(P))
+    s[j++] = desempilha(P);
+  s[j] = '\0';
+  destroip(&P);
+  return s;
+}
+
+int main(void) {
+  char e[513];
+  printf("Infixa booleana: ");
+  fgets(e, sizeof(e), stdin);
+  printf("Posfixa: %s\n", posfixa(e));
+  return 0;
+}
